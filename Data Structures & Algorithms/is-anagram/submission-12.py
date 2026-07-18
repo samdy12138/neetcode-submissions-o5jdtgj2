5dc149cdd8ab022165607f321d9fd7dba07d5721
w12138 <78838171+samdy12138@users.noreply.github.com>
2026-07-18 16:38:31 -0400

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap1={}
+        hashmap2={}
+
+        for ch in s:
+            if ch in hashmap1:
+                hashmap1[ch]+=1
+            else:
+                hashmap1[ch]=1
+        for ch in t:
+            if ch in hashmap2:
+                hashmap2[ch]+=1
+            else:
+                hashmap2[ch]=1
+        return hashmap1==hashmap2
+        '''
+ I use two hashmaps to count characters.
+One hashmap is for string s, and the other one is for string t.
+I count how many times each character appears in both strings.
+Then I compare the two hashmaps.
+If they are the same, I return true.
+If they are different, I return false.
+
+
+ '''
